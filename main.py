0ee36ccff8cd01cebc172f8e6ad885f0da8d7a06
Bohdan Vanieiev <warchantua@gmail.com>
2020-04-25 16:42:16 +0300

    Add one more false positive

--- main.py
+++ main.py
@@ -48,3 +48,16 @@ def true_import_star():
     data = input()
     loaded = load(data)
     print(loaded)
+
+def false_class_dot_load():
+    class Test:
+        def __init__(self):
+            pass
+
+        def load(self, x):
+            return x + "asd"
+
+    data = input()
+    test = Test()
+    loaded = test.load(data)
+    print(loaded)
